e17bd38c84728ca9ee72f1b763bd57b862e286d9
Whithmann Obando <whithmann_98@hotmail.com>
2022-11-29 19:43:02 -0500

    Add: new project

---
+++ src/main/java/pe/idat/TrabajoSemana12Application.java
@@ -0,0 +1,13 @@
+package pe.idat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TrabajoSemana12Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TrabajoSemana12Application.class, args);
+	}
+
+}
---
+++ src/test/java/pe/idat/TrabajoSemana12ApplicationTests.java
@@ -0,0 +1,13 @@
+package pe.idat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TrabajoSemana12ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
